ffd91b721649fc7ada330de84b5b59e13e3d6bdc
sergey_2086 <kvlchk.s@gmail.com>
2022-05-02 17:43:59 +0700

    create components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css'
+
+const Header = () => {
+  return (
+    <header className='app-wrapper__header'>
+      <img alt='logo' src='https://i.pinimg.com/originals/d2/9f/c1/d29fc14e3e508b26c3c121c133df174c.png' />
+      <p>Header</p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+const Navbar = () => {
+  return (
+    <div className='content-wrapper__navbar'>
+      <ul>
+        <li>News</li>
+        <li>Messages</li>
+        <li>Settings</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
