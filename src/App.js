f936ff9034704945129061a3ae82fee543dd5da2
sergey_2086 <kvlchk.s@gmail.com>
2022-05-02 13:43:26 +0700

    lesson 7 - component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,19 @@ function App() {
   return (
     <div className='App'>
       <p className='word'>Hi</p>
+      <Header />
     </div>
   );
+}
+
+function Header() {
+  return (
+    <div>
+      <a href='http://vk.com'>
+        Hello
+      </a>
+    </div>
+  )
 }
 
 export default App;
