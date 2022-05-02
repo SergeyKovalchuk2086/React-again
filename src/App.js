d299c9df9db2781baab4f901f7853cb3a56ac0ae
sergey_2086 <kvlchk.s@gmail.com>
2022-05-02 22:39:00 +0700

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import ContentWrapper from './components/Content-wrapper';
 
 function App() {
