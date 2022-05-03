285b48df58aa5606b698cd7fed0ec11075a694a2
sergey_2086 <kvlchk.s@gmail.com>
2022-05-03 19:48:12 +0700

    navlink

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import './Content.css';
+import './Profile.css';
 import Posts from './Posts/Posts';
 
-const Content = () => {
+const Profile = () => {
   return (
-    <div className='content'>
       <div>
-        <img
-          alt='back'
-          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTioXfAaPnwiLL3ISHXde0LgfVKYI5TYnQQqYEE40G7KYTHOuomz5lsaNZZWiqo-X_9XSM&usqp=CAU'
-        />
-      </div>
+      <img
+      alt='back'
+      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTioXfAaPnwiLL3ISHXde0LgfVKYI5TYnQQqYEE40G7KYTHOuomz5lsaNZZWiqo-X_9XSM&usqp=CAU'
+      />
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, placeat ea! Itaque labore cum, doloribus numquam possimus inventore vitae voluptas soluta animi rerum omnis dignissimos accusantium veniam nesciunt corrupti laborum! </p>
       <Posts />
-    </div>
-  );
-};
+      </div>
+  )
+}
 
-export default Content;
+export default Profile;
