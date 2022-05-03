8a4af4dd9296364e288e7c22564bfc5f120dc633
sergey_2086 <kvlchk.s@gmail.com>
2022-05-03 20:26:54 +0700

    route path

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Dialogs.css'
 
 const Dialogs = () => {
-   return (
-      <div>
-         Dialogs
+  return (
+      <div className='dialogs__container'>
+        <div className='dialogs__items'>
+            <div className='dialog'>
+            	<NavLink to='/dialogs/1'>Bogdan</NavLink>
+            </div>
+            <div className='dialog'>
+              <NavLink to='/dialogs/2'>Alexandra</NavLink>
+            </div>
+        </div>
+
+        <div className='dialogs__messages'>
+            <div className='message'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat molestias voluptatibus possimus, harum alias optio sed quos tempora earum architecto sit nesciunt voluptas! Voluptate laborum aut iure quo veniam voluptas.
+            </div>
+        </div>
       </div>
-   )
+  )
 }
 
 export default Dialogs;
