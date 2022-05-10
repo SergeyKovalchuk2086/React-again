1dbd1e35e3520795fc2ec378da57c9e09176bb83
sergey_2086 <kvlchk.s@gmail.com>
2022-05-10 21:16:33 +0700

    pull

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import state from './state.js'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App dialogs={state.dialogs} messages={state.messages} posts={state.posts}/> */}
     <App state={state}/>
   </React.StrictMode>
 );
